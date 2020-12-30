33e8c2350b8ff2f7830f8c31cb3f07a65db04e1d
Martín Fdez <paziusss@gmail.com>
2020-12-30 20:20:43 +0100

    Remove python wrappers as they're incompatible with py3 fix warnings in samsung debug's KConfig enable DT building in defconfig update build script & add instructions

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SamsungtabA7
+# android_kernel_samsung_gta4lwifi
+Samsung SM-T500 kernel
+
+Based on T500XXU1ATJ2 - SM-T500_EUR_QQ_Opensource.zip.
+
+You'll need the toolchain from the zip, it wasn't included here.
+
+## To build
+
+1. In the file `build_kernel.sh` edit `TOOLCHAIN_PATH`
+2. Run `./build_kernel.sh`
--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+# shellcheck disable=SC2046
 
-export ARCH=arm64
-mkdir out
+# REPLACE ME: Absolute path where the root of the toolchain is located
+TOOLCHAIN_PATH="/home/pazos/Escritorio/SM-T500/toolchain"
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/8.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+# Relative paths for gcc & llvm toolchains
+CROSS_TC="${TOOLCHAIN_PATH}/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+CLANG_BIN="${TOOLCHAIN_PATH}/llvm-arm-toolchain-ship/8.0/bin/clang"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/gta4lwifi_eur_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+function checkTC {
+    if [ ! -f "$1" ]; then
+        echo "File $1 doesn't exist, please check your toolchain path"
+        exit 1
+    fi
+}
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+if [ ! -d $TOOLCHAIN_PATH ]; then
+    echo "Toolchain path doesn't exist"
+    exit 1
+else
+    checkTC "${CROSS_TC}gcc"
+    checkTC "$CLANG_BIN"
+fi
+
+echo "GCC: ${CROSS_TC}gcc"
+echo "CLANG: $CLANG_BIN"
+
+[ -d out ] && rm -rf out
+mkdir -v out
+
+make -j8 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE="$CROSS_TC" REAL_CC="$CLANG_BIN" CLANG_TRIPLE=aarch64-linux-gnu- vendor/gta4lwifi_eur_open_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE="$CROSS_TC" REAL_CC="$CLANG_BIN" CLANG_TRIPLE=aarch64-linux-gnu-
+
